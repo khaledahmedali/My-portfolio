ea6d25b6ec337f655597c0a0646e702f0ee016bc
khaledahmedali <122289488+khaledahmedali@users.noreply.github.com>
2025-01-23 22:54:29 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,3 +12,18 @@ let nav_links =document.getElementById("links");
 function open_colose_menu(){
     nav_links.classList.toggle("active")
 }
+
+let btn = document.getElementById("button1");
+
+window.onscroll = function(){
+  if(window.scrollY >= 600){
+    btn.style.display ="block"
+  }else{ btn.style.display ="none"}
+}
+btn.onclick = function(){
+  window.scrollTo({
+    left:0,
+    top:0,
+    behavior:"smooth"
+  })
+}
